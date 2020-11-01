c8a5d1dfb47c3ae88db9e9e14cc0e755a071c248
ah-james <11ahj11@gmail.com>
2020-11-01 14:57:50 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School
+    attr_accessor :school_name, :roster
+
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = Hash.new{ |k, v| k[v] = []}
+    end
+    
+    def add_student(student_name, grade)
+        @roster[grade] << student_name
+    end
+
+    def grade(year)
+        roster[year]
+    end
+
+    def sort
+        name = {}
+        roster.each do |grade, student|
+            name[grade] = student.sort
+        end
+        name
+    end
+end
+
